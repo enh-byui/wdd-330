87479ada4fe9716a6a48a064191fc0e387b188e9
ramakrishnadas <rama.k.d.b.p@gmail.com>
2024-01-18 18:58:06 -0600

    W02 Team Activity - Completed

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,18 +1,60 @@
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
+
+function productDetailsTemplate(product) {
+    return `<section class="product-detail">
+    <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
+
+// Create myCart variable to store an array of the elements in the cart
+let myCart = [];
+
+function checkCart() {
+  // Check if the cart is not empty. If it's not empty, then push every element in the cart into the myCart array.
+  // If it is empty, do nothing
+  if (localStorage.getItem('so-cart') != null && localStorage.getItem('so-cart') != '') {
+    let currentCart = getLocalStorage('so-cart');
+
+    currentCart.forEach(element => {
+      myCart.push(element);
+    });
+  } else {
+    myCart = [];
+  }
+}
+
 export default class ProductDetails {
     constructor(productId, dataSource) {
       this.productId = productId;
       this.product = { };
       this.dataSource = dataSource;
     }
-    init() {
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
 
+        this.renderProductDetails('main');
+
+        document.getElementById('addToCart').addEventListener('click', this.addToCart.bind(this));
     }
     addToCart(product) {
         // Call checkCart function to check if the cart is empty
         checkCart();
 
         // Push the new product to the myCart array
-        myCart.push(product);
+        myCart.push(this.product);
 
         // Set the cart with the old products and the new product
         setLocalStorage('so-cart', myCart);
@@ -20,13 +62,8 @@ export default class ProductDetails {
         // Empty the myCart array again for future use
         myCart = [];
     }
-    renderProductDetails() {
-        let temp = document.getElementsByTagName('template')[0];
-        let clon = temp.content.importNode(true);
-
-        const brandName = clon.querySelector('h3');
-        brandName.textContent = this.product.Brand.Name;
-
-        document.body.appendChild(clon);
+    renderProductDetails(selector) {
+        const element = document.querySelector(selector);
+        element.insertAdjacentHTML('afterBegin', productDetailsTemplate(this.product));
     }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,3 @@
-import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 import { getParams } from './utils.mjs';
 import ProductDetails from './ProductDetails.mjs';
@@ -8,34 +7,3 @@ const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
-product.renderProductDetails();
-
-
-// Create myCart variable to store an array of the elements in the cart
-let myCart = [];
-
-function checkCart() {
-  // Check if the cart is not empty. If it's not empty, then push every element in the cart into the myCart array.
-  // If it is empty, do nothing
-  if (localStorage.getItem('so-cart') != null && localStorage.getItem('so-cart') != '') {
-    let currentCart = getLocalStorage('so-cart');
-
-    currentCart.forEach(element => {
-      myCart.push(element);
-    });
-  } else {
-    myCart = [];
-  }
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
-
