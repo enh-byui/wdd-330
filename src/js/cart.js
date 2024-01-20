d7cde1f4e2b3a89e72d27283a3add4771965add4
ramakrishnadas <rama.k.d.b.p@gmail.com>
2024-01-20 11:43:28 -0600

    changed double quotes to single quotes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,12 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage('so-cart');
   if (cartItems != null) {
     const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    document.querySelector('.product-list').innerHTML = htmlItems.join('');
   } else {
-    document.querySelector(".product-list").innerHTML = "<p>No items added</p>";
+    document.querySelector('.product-list').innerHTML = '<p>No items added</p>';
   }
 }
 
