0f0d3e7a06fed4d72645a5bd2aab3d02cf1bd19a
Cesar Coutino <ccoutino@gmail.com>
2024-01-26 13:59:29 -0700

    individual activity

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src/',
-  base: '/',
 
   build: {
     outDir: '../dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        listing: resolve(__dirname, "src/product-listing/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
       }, 
     },
