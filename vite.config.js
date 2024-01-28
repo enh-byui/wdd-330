1d2a698174211c321249dc28dfcd23dd57c5f489
Cesar Coutino <ccoutino@gmail.com>
2024-01-27 18:01:02 -0700

    Update files

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,4 @@ export default defineConfig({
       }, 
     },
   },
-
-  // Access environment variables in client-side code
-  define: {
-    VITE_SERVER_URL: JSON.stringify(process.env.VITE_SERVER_URL),
-  },
 });
