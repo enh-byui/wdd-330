766060bd26a52ec16bc57ebcee6bc8e57fccff2f
Cesar Coutino <ccoutino@gmail.com>
2024-01-30 12:52:58 -0700

    Update vit config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   root: 'src/',
@@ -13,8 +10,11 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'src/index.html'),
         cart: resolve(__dirname, 'src/cart/index.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
         checkout: resolve(__dirname, 'src/checkout/index.html'),
+        product: resolve(__dirname, 'src/product_pages/index.html'),
+        header: resolve(__dirname, 'src/public/partials/header.html'),
+        footer: resolve(__dirname, 'src/public/partials/footer.html'),
+        listing: resolve(__dirname, 'src/product-listing/index.html'),
       },
     },
   },
